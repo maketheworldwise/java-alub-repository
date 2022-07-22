83c892e596677523c88643f73ca87c351103ecb2
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-23 02:29:47 +0900

    BOJ-10718 'We love kriii' [ 22/07/23 02:29:47 ] by Alub

---
+++ BOJ/10718/10718_1.java
@@ -0,0 +1,11 @@
+/**
+* 메모리: 13900 KB, 시간: 116 ms
+* 2022.07.23
+* by Alub
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
