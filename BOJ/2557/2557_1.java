87d2daf6872885de59bcd3c969c3de7948e60a68
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-23 02:27:33 +0900

    BOJ-2557 'Hello World' [ 22/07/23 02:27:33 ] by Alub

---
+++ BOJ/2557/2557_1.java
@@ -0,0 +1,11 @@
+/**
+* 메모리: 13812 KB, 시간: 112 ms
+* 풀이 시간: 00:00:00
+* 2022.07.23
+* by Alub
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
