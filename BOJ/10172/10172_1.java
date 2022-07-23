9ca52e4c260c7fbcc5c4ac45bc689b70d0c82867
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-23 15:21:15 +0900

    BOJ-10172 '개' [ 22/07/23 15:21:14 ] by Alub

---
+++ BOJ/10172/10172_1.java
@@ -0,0 +1,21 @@
+/**
+* 메모리: 13976 KB, 시간: 120 ms
+* 2022.07.23
+* by Alub
+*/
+/*
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
