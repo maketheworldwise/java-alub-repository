87e22f1deb316f1415a3bb3b52113b6b3dbfdd3b
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-23 15:17:27 +0900

    BOJ-10171 '고양이' [ 22/07/23 15:17:27 ] by Alub

---
+++ BOJ/10171/10171_1.java
@@ -0,0 +1,13 @@
+/**
+* 메모리: 13960 KB, 시간: 124 ms
+* 2022.07.23
+* by Alub
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( \')");                   
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
