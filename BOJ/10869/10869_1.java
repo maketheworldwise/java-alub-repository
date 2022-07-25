dc7b5666b9477ec1431aed85dc812b2d9101207d
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-25 13:45:51 +0900

    BOJ-10869 '사칙연산' [ 22/07/25 13:45:51 ] by Alub

---
+++ BOJ/10869/10869_1.java
@@ -0,0 +1,20 @@
+/**
+* 메모리: 17748 KB, 시간: 212 ms
+* 2022.07.25
+* by Alub
+*/
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
