fc71b908dc2a2eadc50205cb8a26b461fefdd441
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-25 13:57:34 +0900

    BOJ-18108 '1998년생인 내가 태국에서는 2541년생?!' [ 22/07/25 13:57:33 ] by Alub

---
+++ BOJ/18108/18108_1.java
@@ -0,0 +1,14 @@
+/**
+* 메모리: 17748 KB, 시간: 220 ms
+* 2022.07.25
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println(a-543);
+    }
+}
