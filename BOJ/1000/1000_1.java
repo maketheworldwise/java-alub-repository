57df73cffc9e6a636c031a7bf075218db6150e71
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-25 13:24:55 +0900

    BOJ-1000 'A+B' [ 22/07/25 13:24:54 ] by Alub

---
+++ BOJ/1000/1000_1.java
@@ -0,0 +1,15 @@
+/**
+* 메모리: 17744 KB, 시간: 216 ms
+* 2022.07.25
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a+b);
+    }
+}
