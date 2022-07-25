b5747516352fa4cb36bbce35c18fbd02ce95d9cf
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-25 13:27:37 +0900

    BOJ-1001 'A-B' [ 22/07/25 13:27:36 ] by Alub

---
+++ BOJ/1001/1001_1.java
@@ -0,0 +1,16 @@
+/**
+* 메모리: 17700 KB, 시간: 212 ms
+* 2022.07.25
+* by Alub
+*/
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a-b);
+    }
+}
