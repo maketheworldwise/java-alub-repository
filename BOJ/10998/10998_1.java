0fc2e6976004e7eeceaed03eaaaafe69780f9325
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-25 13:29:19 +0900

    BOJ-10998 'A×B' [ 22/07/25 13:29:19 ] by Alub

---
+++ BOJ/10998/10998_1.java
@@ -0,0 +1,16 @@
+/**
+* 메모리: 17720 KB, 시간: 220 ms
+* 2022.07.25
+* by Alub
+*/
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a*b);
+    }
+}
