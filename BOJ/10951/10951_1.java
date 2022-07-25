8cdbef9cc542288f05a0f1ab32b2fd1d957ca3d6
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-25 21:41:27 +0900

    BOJ-10951 'A+B - 4' [ 22/07/25 21:41:27 ] by Alub

---
+++ BOJ/10951/10951_1.java
@@ -0,0 +1,17 @@
+/**
+* 메모리: 17816 KB, 시간: 228 ms
+* 2022.07.25
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
