ef9b23022a503e271cd225436a00b6ac498676a7
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-25 14:10:51 +0900

    BOJ-25083 '새싹' [ 22/07/25 14:10:51 ] by Alub

---
+++ BOJ/25083/25083_1.java
@@ -0,0 +1,15 @@
+/**
+* 메모리: 13932 KB, 시간: 116 ms
+* 2022.07.25
+* by Alub
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");       
+    }
+}
