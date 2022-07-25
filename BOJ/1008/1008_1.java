8223da0177239629d9568515788d577c58d22431
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-25 13:31:15 +0900

    BOJ-1008 'A/B' [ 22/07/25 13:31:15 ] by Alub

---
+++ BOJ/1008/1008_1.java
@@ -0,0 +1,17 @@
+/**
+* 메모리: 17760 KB, 시간: 216 ms
+* 2022.07.25
+* by Alub
+*/
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println((double)a/b);
+    }
+}
