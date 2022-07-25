3283cec4630dda50920fa54ef045c2742ffd789d
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-25 16:22:17 +0900

    BOJ-11021 'A+B - 7' [ 22/07/25 16:22:17 ] by Alub

---
+++ BOJ/11021/11021_1.java
@@ -0,0 +1,30 @@
+/**
+* 메모리: 16164 KB, 시간: 156 ms
+* 2022.07.25
+* by Alub
+*/
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int t = Integer.parseInt(br.readLine());
+        for(int i = 1; i <= t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write("Case #" + i + ": " + (a+b));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
