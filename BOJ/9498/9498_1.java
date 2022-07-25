4d1737bbd97debbfc28d314b43daf15804f9cd34
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-25 14:18:21 +0900

    BOJ-9498 '시험 성적' [ 22/07/25 14:18:21 ] by Alub

---
+++ BOJ/9498/9498_1.java
@@ -0,0 +1,19 @@
+/**
+* 메모리: 17712 KB, 시간: 212 ms
+* 2022.07.25
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if(a >= 90 && a <= 100) System.out.println("A");
+        else if(a >= 80 && a <= 89) System.out.println("B");
+        else if(a >= 70 && a <= 79) System.out.println("C");
+        else if(a >= 60 && a <= 69) System.out.println("D");
+        else System.out.println("F");
+        scanner.close();
+    }
+}
