840cff9b7ba9d1672541fda56943dc74be0f7546
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-25 14:01:42 +0900

    BOJ-10430 '나머지' [ 22/07/25 14:01:41 ] by Alub

---
+++ BOJ/10430/10430_1.java
@@ -0,0 +1,20 @@
+/**
+* 메모리: 17712 KB, 시간: 204 ms
+* 2022.07.25
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+        scanner.close();
+    }
+}
