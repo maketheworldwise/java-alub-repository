3b9c8f2959822557f64d221279803f4590a17de6
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-25 14:07:16 +0900

    BOJ-2588 '곱셈' [ 22/07/25 14:07:15 ] by Alub

---
+++ BOJ/2588/2588_1.java
@@ -0,0 +1,19 @@
+/**
+* 메모리: 17752 KB, 시간: 212 ms
+* 2022.07.25
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a*(b%10));
+        System.out.println(a*((b/10)%10));
+        System.out.println(a*(b/100));
+        System.out.println(a*b);
+        scanner.close();
+    }
+}
