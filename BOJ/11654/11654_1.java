628ca70512b3fdd363a4292cd7a7aa9cb9e32e06
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-07-27 12:06:42 +0900

    BOJ-11654 '아스키 코드' [ 22/07/27 12:06:41 ] by Alub

---
+++ BOJ/11654/11654_1.java
@@ -0,0 +1,14 @@
+/**
+* 메모리: 17600 KB, 시간: 216 ms
+* 2022.07.27
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println((byte)scanner.next().charAt(0));
+        scanner.close();
+    }
+}
