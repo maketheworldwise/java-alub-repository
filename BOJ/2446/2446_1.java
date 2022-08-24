52cc55bbaf3ab09a12f49eb7bf8172e0e38aa4a3
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-08-24 13:35:10 +0900

    BOJ-2446 '별 찍기 - 9' [ 22/08/24 13:35:10 ] by Alub

---
+++ BOJ/2446/2446_1.java
@@ -0,0 +1,34 @@
+/**
+* 메모리: 23348 KB, 시간: 480 ms
+* 2022.08.24
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+
+		for(int i = n; i > 0; i--) {
+			for(int j = i; j < n; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 1; j <= 2*i-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i = 2; i <= n; i++) {
+			for(int j = i; j < n; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 1; j <= 2*i-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		scanner.close();
+    }
+}
