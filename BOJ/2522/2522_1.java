b58622427d9d829d222760a6c4be441da28f736c
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-08-24 12:39:56 +0900

    BOJ-2522 '별 찍기 - 12' [ 22/08/24 12:39:56 ] by Alub

---
+++ BOJ/2522/2522_1.java
@@ -0,0 +1,34 @@
+/**
+* 메모리: 24052 KB, 시간: 504 ms
+* 2022.08.24
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+
+		for(int i = 1; i <= n; i++) {
+			for(int j = i; j < n; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = i; j < n; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		scanner.close();
+    }
+}
